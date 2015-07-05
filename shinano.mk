5f73609e4631b705effd7308c50d7d813fb56290
marlontoe <inclusionrec@googlemail.com>
2015-07-05 19:57:18 +0200

    disabled quickboot
    Change-Id: I5206c8c2439552009e332bc79afa99de855322bb

--- shinano.mk
+++ shinano.mk
@@ -189,12 +189,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.virtual=0
 
 # QuickBoot
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/prebuilt/QuickBoot.apk:system/priv-app/QuickBoot/QuickBoot.apk
+#PRODUCT_COPY_FILES += \
+#    $(COMMON_PATH)/prebuilt/QuickBoot.apk:system/priv-app/QuickBoot/QuickBoot.apk
 
 # QuickBoot Config
-PRODUCT_PROPERTY_OVERRIDES += \
-    config.enable_quickboot=true
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    config.enable_quickboot=true
     
 # Enable USB OTG interface
 PRODUCT_PROPERTY_OVERRIDES += \
