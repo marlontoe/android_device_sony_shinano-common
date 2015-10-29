c8cf8e995bc9500847f5a1c9b9e7ae19695dd297
marlontoe <inclusionrec@googlemail.com>
2015-10-29 11:09:29 +0100

    use z3c fstab for twrp
    Change-Id: I94355cd435555cf7444c026c3c68139aa2e41f7d

--- shinano.mk
+++ shinano.mk
@@ -40,7 +40,6 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
-    $(COMMON_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
